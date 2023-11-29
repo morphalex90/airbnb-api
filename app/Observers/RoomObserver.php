d85ae9d8f9b6b3d4424f635e70ddfb8bb724fe86
morphalex90 <piero.nanni@gmail.com>
2023-11-29 23:34:11 +0000

    remove Emoji from room name

--- app/Observers/RoomObserver.php
+++ app/Observers/RoomObserver.php
@@ -21,6 +21,6 @@ class RoomObserver
 
         $room->slug = $helper->generateUniqueSlug($room->id, $room->name, 'rooms');
         $room->key = Str::uuid(36);
-        $room->name = Str::trim($room->name);
+        $room->name = $helper->removeEmoji(trim($room->name));
     }
 }
