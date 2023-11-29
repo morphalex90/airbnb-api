5538c9281b7b87082c8c057a8b12124a143b91ca
morphalex90 <piero.nanni@gmail.com>
2023-11-29 19:46:11 +0000

    add user and room routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+
+
+    ##### Rooms
+    Route::post('rooms', [RoomController::class, 'store'])->name('rooms.store');
+    Route::get('rooms/{room_slug}', [RoomController::class, 'show'])->name('rooms.show');
+
+    // Route::middleware('auth:api')->group(function () {
+    // });
+
+
+    #### User
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [AuthController::class, 'register']);
+
+        Route::middleware('auth:api')->group(function () {
+            Route::get('user', [UserController::class, 'user']);
+            Route::patch('update', [UserController::class, 'update']);
+            Route::get('logout', [AuthController::class, 'logout']);
+        });
+
+        // Confirm user email
+        Route::get('email/verify', function () {
+            return '';
+        })->middleware('auth')->name('verification.notice');
+        Route::get('account/verify/{id}', [AuthController::class, 'verifyAccount'])->name('verification.verify');
+
+        // Password reset
+        Route::get('account/password', function () {
+            return '';
+        })->middleware('guest')->name('password.request');
+        Route::post('password/reset', [AuthController::class, 'sendPasswordReset'])->middleware('guest')->name('password.email');
+        Route::get('reset-password/{token}', function ($token) {
+            return '';
+        })->middleware('guest')->name('password.reset');
+        Route::post('password/reset/confirm', [AuthController::class, 'saveNewPassword'])->middleware('guest')->name('password.update');
+    });
 });
