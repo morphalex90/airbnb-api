5a782e6d0d70bc87c97a0c5d2b01e198806b5df9
morphalex90 <piero.nanni@gmail.com>
2023-11-29 23:34:29 +0000

    load rooms even on parent cities

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\City;
+use App\Models\Room;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -41,16 +42,28 @@ class CityController extends Controller
     public function show(Request $request): JsonResponse
     {
         $sub_cities = [];
+        $city_ids = [];
+
         $city = City::where('slug', $request->route('city_slug'))->first();
-        if ($city) {
-            if ($city->parent == null) { // it's a parent city! let's load all child!
-                $sub_cities = City::where('parent', $city->id)->orderBy('name', 'ASC')->paginate(50, ['*'], 'p');
-            } else {
-                $city->setRelation('rooms', $city->rooms()->paginate(50, ['*'], 'p'));
-            }
-            return response()->json(['city' => $city, 'sub_cities' => $sub_cities], 200);
+        if (!$city) {
+            return response()->json(['message' => 'City not found'], 404);
         }
-        return response()->json(['message' => 'City not found'], 404);
+
+        $city_ids[] = $city->id;
+        if ($city->parent == null) { // it's a parent city! let's load all child cities!
+            $sub_cities = City::withCount('rooms as rooms')->where('parent', $city->id)->orderBy('name', 'ASC')->get();
+            $city_ids = $sub_cities->pluck('id')->toArray();
+        }
+
+        $rooms = Room::with('city') // load rooms
+            ->whereHas('city', function ($q) use ($city_ids) {
+                $q->whereIn('city_id', $city_ids);
+            })
+            ->orderBy('name', 'ASC')
+            ->paginate(30, ['*'], 'p');
+
+
+        return response()->json(['city' => $city, 'sub_cities' => $sub_cities, 'rooms' => $rooms], 200);
     }
 
     // /**
